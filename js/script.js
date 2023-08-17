2ca4ad48317df8a02c0f554b02c5de8d2cf329f9
Charlene <cerazo87@yahoo.com>
2023-08-17 10:25:39 -0400

    Added innerText to button

--- js/script.js
+++ js/script.js
@@ -4,7 +4,22 @@ var button = document.querySelector(".show-cat");
 var cat = document.querySelector(".cat");
 //console.log(cat);
 
-button.addEventListener("click", function() {
+//When user clicks on the button, function is triggered to show cat image
+//button.addEventListener("click", function() {
     //console.log("It's working!");
-    cat.classList.add("show");
+    //cat.classList.add("show");
+//});
+
+button.addEventListener("click", function() {
+    if (cat.classList.contains("show")) {
+    //console.log("Yes");
+    cat.classList.remove("show");
+    button.innerText = "Wait, come back!";
+    button.classList.add("disappear");
+    } else { 
+        cat.classList.add("show");
+        button.innerText = "Shoo, cat!";
+        button.classList.remove("disappear");
+        //console.log("No");
+}
 });
